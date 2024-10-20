bdd6ce3547e37953434550e1e47cedc42f91f2eb
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-10-20 19:29:44 +0200

    feat: add groups

--- src/01-average-load-tests.ts
+++ src/01-average-load-tests.ts
@@ -1,4 +1,4 @@
-import { sleep } from 'k6';
+import { group, sleep } from 'k6';
 import { Options } from 'k6/options';
 import { randomIntBetween, randomString } from './utils/k6-utils';
 import { host } from './models/test-data';
@@ -21,61 +21,85 @@ export const options: Options = {
       executor: 'ramping-vus',
       exec: 'scenario2',
       stages: [
-        { duration: '10s', target: 40 },
         { duration: '20s', target: 40 },
-        { duration: '5s', target: 0 },
+        { duration: '2m', target: 40 },
+        { duration: '10s', target: 0 },
       ],
     },
   },
 };
 
 export function scenario1() {
-  getSets(host, user1AccessToken);
-  sleep(randomIntBetween(1, 3) / 0.1);
-
-  getSets(host, user1AccessToken, 0, 25, 'createdAt', 'asc');
-  sleep(randomIntBetween(1, 3) / 0.1);
-
-  const createdSetId = createSet(host, user1AccessToken, {
-    entries: [
-      { word: 'word1', wordType: 'noun', translations: ['słowo1'] },
-      { word: 'word2', wordType: 'noun', translations: ['słowo2'] },
-      { word: 'word3', wordType: 'noun', translations: ['słowo3'] },
-    ],
-    setName: randomString(20),
+  group('getSets', () => {
+    getSets(host, user1AccessToken);
+    sleep(randomIntBetween(1, 3) / 0.1);
   });
-  sleep(randomIntBetween(1, 3) / 0.1);
 
-  updateSet(host, user1AccessToken, {
-    entries: [
-      { word: 'word1', wordType: 'noun', translations: ['słowo1'] },
-      { word: 'word2', wordType: 'noun', translations: ['słowo2'] },
-    ],
-    setName: randomString(20),
-    setId: createdSetId,
+  group('getSets with sorting', () => {
+    getSets(host, user1AccessToken, 0, 25, 'createdAt', 'asc');
+    sleep(randomIntBetween(1, 3) / 0.1);
   });
-  sleep(randomIntBetween(1, 3) / 0.1);
 
-  deleteSet(host, user1AccessToken, createdSetId);
-  sleep(randomIntBetween(1, 3) / 0.1);
+  const createdSetId = group('createSet', () => {
+    const createdSetId = createSet(host, user1AccessToken, {
+      entries: [
+        { word: 'word1', wordType: 'noun', translations: ['słowo1'] },
+        { word: 'word2', wordType: 'noun', translations: ['słowo2'] },
+        { word: 'word3', wordType: 'noun', translations: ['słowo3'] },
+      ],
+      setName: randomString(20),
+    });
+    sleep(randomIntBetween(1, 3) / 0.1);
+
+    return createdSetId;
+  });
+
+  group('updateSet', () => {
+    updateSet(host, user1AccessToken, {
+      entries: [
+        { word: 'word1', wordType: 'noun', translations: ['słowo1'] },
+        { word: 'word2', wordType: 'noun', translations: ['słowo2'] },
+      ],
+      setName: randomString(20),
+      setId: createdSetId,
+    });
+    sleep(randomIntBetween(1, 3) / 0.1);
+  });
+
+  group('deleteSet', () => {
+    deleteSet(host, user1AccessToken, createdSetId);
+    sleep(randomIntBetween(1, 3) / 0.1);
+  });
 }
 
 export function scenario2() {
-  getSets(host, user2AccessToken);
-  sleep(randomIntBetween(1, 3) / 0.1);
-
-  getSets(host, user2AccessToken, 0, 25, 'setId', 'desc', 'set3');
-  sleep(randomIntBetween(1, 3) / 0.1);
-
-  const createdSetId = createSet(host, user2AccessToken, {
-    entries: [{ word: 'word1', wordType: 'noun', translations: ['słowo1'] }],
-    setName: randomString(20),
+  group('getSets', () => {
+    getSets(host, user2AccessToken);
+    sleep(randomIntBetween(1, 3) / 0.1);
   });
-  sleep(randomIntBetween(1, 3) / 0.1);
 
-  getSet(host, user2AccessToken, createdSetId);
-  sleep(randomIntBetween(1, 3) / 0.1);
+  group('getSets with searching', () => {
+    getSets(host, user2AccessToken, 0, 25, 'setId', 'desc', 'set3');
+    sleep(randomIntBetween(1, 3) / 0.1);
+  });
 
-  deleteSet(host, user2AccessToken, createdSetId);
-  sleep(randomIntBetween(1, 3) / 0.1);
+  const createdSetId = group('createSet', () => {
+    const createdSetId = createSet(host, user2AccessToken, {
+      entries: [{ word: 'word1', wordType: 'noun', translations: ['słowo1'] }],
+      setName: randomString(20),
+    });
+    sleep(randomIntBetween(1, 3) / 0.1);
+
+    return createdSetId;
+  });
+
+  group('getSet', () => {
+    getSet(host, user2AccessToken, createdSetId);
+    sleep(randomIntBetween(1, 3) / 0.1);
+  });
+
+  group('deleteSet', () => {
+    deleteSet(host, user2AccessToken, createdSetId);
+    sleep(randomIntBetween(1, 3) / 0.1);
+  });
 }
